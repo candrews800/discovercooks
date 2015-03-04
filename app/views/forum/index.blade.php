7bb9486e08813e570a925509964a614bed280d83
Clinton Andrews <candrews800@gmail.com>
2015-03-04 10:43:20 -0500

    bug fixes

--- app/views/forum/index.blade.php
+++ app/views/forum/index.blade.php
@@ -8,10 +8,10 @@
                 <div class="row">
                     <?php $count = 0; ?>
                     @foreach($topics as $key => $topic)
+                        @if($topic->category_id == $category->id)
                         @if($count % 3 == 0)
                             <div class="clearfix">
                         @endif
-                        @if($topic->category_id == $category->id)
                             <div class="col-xs-12 col-sm-6 col-md-4">
                                 <div class="topic">
                                     <h3><a href="{{ url('forum/'.str_replace(' ', '-', $topic->name)) }}">{{ $topic->name }}</a></h3>
@@ -19,9 +19,9 @@
                                 </div>
                             </div>
                             <?php $count++ ?>
-                        @endif
                         @if($count % 3 == 0 && $count > 0 || $key == sizeof($topics)-1)
                             </div>
+                        @endif
                         @endif
                     @endforeach
                 </div>

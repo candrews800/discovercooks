a1282606d2db2c06ea7cc02fcd61b2d93bd45210
Clinton Andrews <candrews800@gmail.com>
2015-03-26 12:49:58 -0400

    updates

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -28,38 +28,36 @@
                 </a>
             </div>
         @endif
-            @if($category->related_recipe_id != 0)
-                <div class="secondary-slider clearfix">
-                    <a class="recipe-category" href="{{ url('category/'.$category->name) }}">
-                        <img src="{{ url('category_images/'.$category->image) }}" />
-                        <span>{{ $category->name }}</span>
-                    </a>
-                </div>
-            @endif
-            @if($category->related_recipe_id != 0)
-                <div class="secondary-slider clearfix">
-                    <a class="recipe-category" href="{{ url('category/'.$category->name) }}">
-                        <img src="{{ url('category_images/'.$category->image) }}" />
-                        <span>{{ $category->name }}</span>
-                    </a>
-                </div>
-            @endif
-            @if($category->related_recipe_id != 0)
-                <div class="secondary-slider clearfix">
-                    <a class="recipe-category" href="{{ url('category/'.$category->name) }}">
-                        <img src="{{ url('category_images/'.$category->image) }}" />
-                        <span>{{ $category->name }}</span>
-                    </a>
-                </div>
-            @endif
-            @if($category->related_recipe_id != 0)
-                <div class="secondary-slider clearfix">
-                    <a class="recipe-category" href="{{ url('category/'.$category->name) }}">
-                        <img src="{{ url('category_images/'.$category->image) }}" />
-                        <span>{{ $category->name }}</span>
-                    </a>
-                </div>
-            @endif
+    @endforeach
+    @foreach($categories as $key=>$category)
+        @if($category->related_recipe_id != 0)
+            <div class="secondary-slider clearfix">
+                <a class="recipe-category" href="{{ url('category/'.$category->name) }}">
+                    <img src="{{ url('category_images/'.$category->image) }}" />
+                    <span>{{ $category->name }}</span>
+                </a>
+            </div>
+        @endif
+    @endforeach
+    @foreach($categories as $key=>$category)
+        @if($category->related_recipe_id != 0)
+            <div class="secondary-slider clearfix">
+                <a class="recipe-category" href="{{ url('category/'.$category->name) }}">
+                    <img src="{{ url('category_images/'.$category->image) }}" />
+                    <span>{{ $category->name }}</span>
+                </a>
+            </div>
+        @endif
+    @endforeach
+    @foreach($categories as $key=>$category)
+        @if($category->related_recipe_id != 0)
+            <div class="secondary-slider clearfix">
+                <a class="recipe-category" href="{{ url('category/'.$category->name) }}">
+                    <img src="{{ url('category_images/'.$category->image) }}" />
+                    <span>{{ $category->name }}</span>
+                </a>
+            </div>
+        @endif
     @endforeach
 </div>
 

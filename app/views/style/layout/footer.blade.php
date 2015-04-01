4bc71ed4a2ff9e7f05e4daed559203ce35a6fbac
Clinton Andrews <candrews800@gmail.com>
2015-04-01 12:54:19 -0400

    updates

--- app/views/style/layout/footer.blade.php
+++ app/views/style/layout/footer.blade.php
@@ -56,10 +56,10 @@
                 <div class="col-xs-12 col-md-6 text-right text-center-xs text-center-sm">
                     <p>By accessing this site, you agree to our <a href="{{ url('terms') }}">Terms of Service</a>.</p>
                 </div>
-                <div class="col-xs-12 col-md-6 text-center-xs text-center-sm">
+                <div class="col-xs-12 col-md-6 text-center-xs text-center-sm col-xs-push-12">
                     <p><em class="text-muted">v.1.0</em></p>
                 </div>
-                <div class="col-xs-12 col-md-6 text-right text-center-xs text-center-sm">
+                <div class="col-xs-12 col-md-6 text-right text-center-xs text-center-sm col-xs-pull-12">
                     <p>All rights reserved.</p>
                 </div>
             </div>

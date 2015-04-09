0ae7e5e31350be4d013c0389764e85ab71abe816
Clinton Andrews <candrews800@gmail.com>
2015-04-09 17:03:44 -0400

    updates

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -57,7 +57,7 @@
                     @endforeach
                 </div>
             </div>
-            <div class="hidden-xs hidden-sm col-md-3">
+            <div class="hidden-xs hidden-sm col-md-3" style="max-height: 403px;">
                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <!-- Responsive -->
                 <ins class="adsbygoogle"
@@ -102,7 +102,7 @@
             <div id="search-results" class="col-xs-12">
                 <div class="row">
                     @foreach($recipes as $key=>$recipe)
-                        @if($key == 6 || $key== 12 || $key==18)
+                        @if($key == 6 || $key== 12)
                             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 masonry-item">
                                 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                                 <!-- Responsive -->

1c3ee3619126c725f5bdce0b3aa1d52bf4528531
Clinton Andrews <candrews800@gmail.com>
2015-04-20 18:03:33 -0400

    updates

--- app/views/blog/index.blade.php
+++ app/views/blog/index.blade.php
@@ -4,13 +4,13 @@
 @section('content')
 
     @foreach($recent_posts as $key=>$post)
-        <h1><a href="{{ url('blog/'.$post->id) }}">{{ $post->title }}</a></h1>
+        <h1><a href="{{ url('blog/'.$post->slug) }}">{{ $post->title }}</a></h1>
         <p class="text-muted"><em>Posted by <a href="{{ url('profile/discovercooks') }}">DiscoverCooks</a> on {{ $post->created_at->format('M d, Y') }}</em></p>
 
         {{ nl2br($post->text) }}
 
         <p>
-            <a href="{{ url('blog/'.$post->id) }}#comment">See Comments</a>
+            <a href="{{ url('blog/'.$post->slug) }}#comment">See Comments</a>
         </p>
         @if($key < sizeof($recent_posts)-1)
         <hr />

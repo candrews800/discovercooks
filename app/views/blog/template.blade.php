95e3e2c663f01ef806c660c065e1095fe95f9357
Clinton Andrews <candrews800@gmail.com>
2015-04-26 19:56:02 -0400

    updates

--- app/views/blog/template.blade.php
+++ app/views/blog/template.blade.php
@@ -3,10 +3,10 @@
 @include ('style.layout.header')
 <div class="container-fluid">
     <div class="row">
-        <div id="breadcrumbs" class="content-top col-xs-12 col-lg-8 col-lg-offset-2">
+        <div id="breadcrumbs" class="content-top col-xs-12">
             @yield('breadcrumbs', ViewHelper::getNewBreadcrumbs(null, 'Blog'))
         </div>
-        <div class="col-xs-12 col-lg-8 col-lg-offset-2">
+        <div class="col-xs-12 col-md-8">
             @yield('content')
             <p class="text-center">
                 <a href="#breadcrumbs">
@@ -14,7 +14,7 @@
                 </a>
             </p>
         </div>
-        <div class="col-xs-12 col-lg-3 col-lg-offset-1">
+        <div class="col-xs-12 col-md-4">
             <h4>RECENT POSTS</h4>
             <ul class="list-unstyled">
                 @foreach($recent_posts as $post)
@@ -22,7 +22,7 @@
                 @endforeach
             </ul>
         </div>
-        <div class="col-xs-12 col-lg-3 col-lg-offset-1">
+        <div class="col-xs-12 col-md-4">
             <h4>ARCHIVE</h4>
             <ul class="list-unstyled">
                 @foreach($archive as $period)
